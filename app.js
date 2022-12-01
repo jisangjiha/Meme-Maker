83f8ce322142979602386358c60686602a5752d4
Jisang Hong <jisang1024@gmail.com>
2022-12-01 15:28:21 +0900

    mouse-painting

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.stroke();
+let isPainting = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleava", cancelPainting);

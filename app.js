c3f886bdba3641dd6525616b806f1858059c8383
Jisang Hong <jisang1024@gmail.com>
2022-12-02 15:30:58 +0900

    line-width

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const canvas = document.querySelector("canvas");
+const lineWidth = document.getElementById("line-width");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
@@ -12,6 +13,7 @@ function onMove(event) {
     ctx.stroke();
     return;
   }
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 function startPainting() {
@@ -20,8 +22,13 @@ function startPainting() {
 function cancelPainting() {
   isPainting = false;
 }
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleava", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
